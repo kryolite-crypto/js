1ad26b3616de8fc6533e34cae03f973eab047973
De-Crypted <email@example.com>
2023-03-21 11:54:42 +0200

    Some comments

--- samples/test.js
+++ samples/test.js
@@ -2,16 +2,19 @@ const Kryolite =  require('../lib/kryolite.cjs');
 
 async function test()
 {
+    // create new address
     const address = await Kryolite.Address.Create();
     console.log("private key: " + address.PrivateKey);
     console.log("public key: " + address.PublicKey);
     console.log("address: " + address.Address);
 
+    // import address from private key
     const imported = await Kryolite.Address.Import(address.PrivateKey);
     console.log("private key: " + imported.PrivateKey);
     console.log("public key: " + imported.PublicKey);
     console.log("address: " + imported.Address);
 
+    // Create and sign transaction
     const transaction = new Kryolite.Transaction();
     transaction.TransactionType = Kryolite.TransactionType.PAYMENT;
     transaction.PublicKey = address.PublicKey;
